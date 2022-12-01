0d10f676516e5c8659dc3e5f383a9f4a78884aff
Gordon <gburgett@watermark.org>
2022-12-01 12:09:14 -0600

    Get running app

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@
  * This is a general purpose Gradle build.
  * Learn more about Gradle by exploring our samples at https://docs.gradle.org/7.6/samples
  */
+
+allprojects {
+  repositories {
+    jcenter()
+  }
+}
